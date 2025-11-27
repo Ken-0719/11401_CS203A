72147dc351ef88453782be9aac8e298d17c7fca1
Ken-0719 <hboyan309@gmail.com>
2025-11-27 22:51:38 +0800

    11/27作業繳交

--- AssignmentIV/C/hash_fn.c
+++ AssignmentIV/C/hash_fn.c
@@ -13,15 +13,32 @@
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
  */
 
+ /*
+ 所用hash function是參考別人的Knuth乘法法(使用黃金分割數但只取到0.618)以及djb2字串hash function
+ 參考來源:
+ https://www.google.com/search?q=knuth+%E4%B9%98%E6%B3%95+hashing&oq=&gs_lcrp=EgZjaHJvbWUqCQgCECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMg8IBxAuGCcYxwEY6gIY0QPSAQkxODc5ajBqMTWoAgiwAgHxBVFqJUFVb44r8QVRaiVBVW-OKw&sourceid=chrome&ie=UTF-8
+ 
+ https://www.reddit.com/r/cs50/comments/ggvgvm/pset5_djb2_hash_function/?tl=zh-hant
+ */ 
+
 #include "hash_fn.h"
 
 int myHashInt(int key, int m) {
     // TODO: replace with your own design
-    return key % m;  // division method example
+    float k = key*0.618;
+    k = k - (int)k;
+
+    return (int)(m * k);  // multiplication method example
 }
 
 int myHashString(const char* str, int m) {
-    unsigned long hash = 0;
+    unsigned long hash = 5381;
     // TODO: replace with your own design
+
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
     return (int)(hash % m); // basic division method
 }
--- AssignmentIV/CXX/hash_fn.cpp
+++ AssignmentIV/CXX/hash_fn.cpp
@@ -12,15 +12,32 @@
 
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
  */
+
+ /*
+ 所用hash function是參考別人的Knuth乘法法(使用黃金分割數但只取到0.618)以及djb2字串hash function
+ 參考來源:
+ https://www.google.com/search?q=knuth+%E4%B9%98%E6%B3%95+hashing&oq=&gs_lcrp=EgZjaHJvbWUqCQgCECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMg8IBxAuGCcYxwEY6gIY0QPSAQkxODc5ajBqMTWoAgiwAgHxBVFqJUFVb44r8QVRaiVBVW-OKw&sourceid=chrome&ie=UTF-8
+ 
+ https://www.reddit.com/r/cs50/comments/ggvgvm/pset5_djb2_hash_function/?tl=zh-hant
+ */ 
+
 #include "hash_fn.hpp"
 
 int myHashInt(int key, int m) {
     // TODO: replace with your own design
-    return key % m;  // basic division method
+
+    const double A = 0.6180339887;  // (sqrt(5) - 1) / 2
+    double frac = (key * A) - static_cast<long long>(key * A);
+    return static_cast<int>(m * frac);
 }
 
 int myHashString(const std::string& str, int m) {
-    unsigned long hash = 0;
+    unsigned long hash = 5381;
     // TODO: replace with your own design
+
+    for (char c : str) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
     return static_cast<int>(hash % m);  // basic division method
 }
